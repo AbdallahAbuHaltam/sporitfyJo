f63ef57594feee0ff6592a0deb4d7fae08ea0966
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-09-25 22:00:44 +0300

    Added Colors

---
+++ lib/utilities/utilities.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+//Light Mode Colors
+const  Color mBackgroundColor = Color(0xFFFEF4E8);
+const  Color mPrimaryColor=Color(0xFF6E2646);
+const  Color mSecondaryColor=Color(0xFFEBE1D5);
+
+//Dark Mode Colors
+const  Color dBackgroundColor = Color(0xFF121212);
+const  Color dPrimaryColor=Color(0xFF6E2646);
+const  Color dSecondaryColor=Color(0xFF2D2D2D);
